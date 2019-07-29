db1f36c7ccaf6406c5a316ccba872f7188af36a0
Kelley Chaplain <kmchaplain@gmail.com>
2019-07-28 21:11:16 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  belongs_to :network
+
+  def actors_list
+    characters.collect do |person|
+      "#{person.actor.full_name}"
+    end  
+  end  
   
 end
